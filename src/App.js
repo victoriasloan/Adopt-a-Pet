a91ef9a96685e1ca085e41aab829d1af5920f3e0
Victoria Sloan <victoriasloan95@gmail.com>
2018-12-10 22:30:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h3", {}, props.breed)
+    ]);
+}
+
+class App extends React.Component {
+    handleTitleClick() {
+        alert("you clicked the title");
+    }
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement('h1', { onClick: this.handleTitleClick }, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Larry",
+                animal: "Dog",
+                breed: "Lhasa Apso"
+            }),
+            React.createElement(Pet, {
+                name: "Paul",
+                animal: "Cat",
+                breed: "Ginger"
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Parrot"
+            })
+        ]) 
+    }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))

142823ad9a2fcddd626054b1a5a5469c73c45a34
Victoria Sloan <victoriasloan95@gmail.com>
2018-12-18 23:53:59 +0000

    pets with images, location and breed info

--- src/App.js
+++ src/App.js
@@ -66,6 +66,8 @@ class App extends React.Component {
                 animal={pet.animal}
                 name={pet.name}
                 breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
               />
             );
           })}
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,31 @@
 import React from "react";
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.animal}</h1>
-      <h1>{props.breed}</h1>
-    </div>
-  );
-};
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    //weird API GRR =[
+    if (media && media.photos && media.photos.photo) {
+      //only use the photo with @size as it returns a few
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
